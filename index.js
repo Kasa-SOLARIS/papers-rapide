f3138df2eb52d18ce7447196a3682dbc323256b9
Kasa-SOLARIS <53957144+Kasa-SOLARIS@users.noreply.github.com>
2019-08-16 16:38:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on ('message', message => {
         .addField('$Invite',"Envoie un lien pour invité le bot Ou [Click Ici](https://discordapp.com/api/oauth2/authorize?client_id=606165721902350356&permissions=27649&scope=bot) ", true)
         .addField('$Ping',"Affiche le ping du bot.")
         .setColor('0xff0101')
-        .setFooter('Created By 7igr4d3#7395')
+        .setFooter('Created By Kasa#0400')
   message.channel.sendEmbed(embed);
   }
   })
@@ -65,7 +65,7 @@ bot.on("message", msg => {
                    if(!msg.guild.member(msg.author).hasPermission("ADMINISTRATOR")) return msg.channel.send(":x: MP Annulé, Tu n'as pas la permission d'utiliser cette commande!");
                    if(!msge) return msg.channel.send("Précise un message")
                    var mpall =new Discord.RichEmbed()
-                        .addField("Message:", msge);
+                        
          
                    msg.delete()
                    msg.guild.members.map(m => m.send(mpall))
